9731c711d58762e94065d6965e465d9ea84af568
tuomasvaltanenLUAS <63774990+tuomasvaltanenLUAS@users.noreply.github.com>
2020-04-16 12:09:30 +0300

    Update application.py

--- application.py
+++ application.py
@@ -1,7 +1,5 @@
-import sys
-
 list_of_stuff = ['asia1','asia2','asia3','asia4','asia5','asia6','asia7','asia8','asia9', 'asia10']
-how_many = int(sys.argv[1])
+how_many = int(input("KUINKA MONTA?:"))
 
 # Parameter "how_many" is an integer, and is used to get that number of items from the list "list_of_stuff
 # TASK: print a string joined by ",". the joined items are from the list_of_stuff dictated by the value of "how_many"
