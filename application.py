801cd457367a88a05c044d8096c04c7214c0b03b
tuomasvaltanenLUAS <63774990+tuomasvaltanenLUAS@users.noreply.github.com>
2020-04-16 10:33:24 +0300

    Create application.py

---
+++ application.py
@@ -0,0 +1,10 @@
+import sys
+
+list_of_stuff = ['asia1','asia2','asia3','asia4','asia5','asia6','asia7','asia8','asia9', 'asia10']
+how_many = int(sys.argv[1])
+
+# Parameter "how_many" is an integer, and is used to get that number of items from the list "list_of_stuff
+# TASK: print a string joined by ",". the joined items are from the list_of_stuff dictated by the value of "how_many"
+# Print the result in the end of the script.
+# Example output: with 3, the output should be "asia1, asia2, asia3", etc.
+
